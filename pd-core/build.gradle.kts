0c71cb62693e081250d0a10bb39beffb97e6b388
杜金亮 <dujinliang9@163.com>
2023-07-03 22:44:59 +0800

    "feat(sub): web服务器从tomcat替换为undertow"

--- pd-core/build.gradle.kts
+++ pd-core/build.gradle.kts
@@ -6,10 +6,14 @@ plugins {
 description = "核心模块"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-undertow")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-discovery")
     implementation("com.alibaba.cloud:spring-cloud-starter-alibaba-nacos-config")
+    implementation("com.github.xiaoymin:knife4j-spring-boot-starter")
     implementation("cn.hutool:hutool-all")
     implementation("org.slf4j:slf4j-api")
     implementation("com.alibaba:fastjson")
